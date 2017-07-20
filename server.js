5762f1e32697442266a7a8f15536503d2226acbc
mmaines-cisco <mmaines@cisco.com>
2017-07-20 15:25:30 -0400

    edit

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ let auth = function(req, res, next) {
     else
     {
         //https://api.ciscospark.com/v1/authorize?response_type=code&client_id=Cf14c66a547d565a58a107141a509e8f4d8a7367e7a9cd9966675387841c41ca9&scope=spark:rooms_read,spark:messages_write&state=youbetcha&redirect_uri=https://requestb.in/zh18zhzh
-        const url = `https://api.ciscospark.com/v1/authorize?response_type=code&client_id=` + env.ciscospark["client-id"] + `&scope=` + env.ciscospark.scope + `&redirect_uri=` + env.ciscospark['redirect-uri'];
+        const url = `https://api.ciscospark.com/v1/authorize?response_type=code&client_id=` + env.ciscospark["client-id"] + `&scope=` + env.ciscospark.scope + `&redirect_uri=` + env.ciscospark['redirect-uri'] + '/test';
         //console.log(url);
         res.redirect(url);
         //console.log("HELOOOO");
@@ -71,13 +71,15 @@ server.get('/test', auth, function(req, res) {
     res.send(200);
 });
 
-server.post('/shopify/access_code', function(req, res) {
-    console.log(res.query);
-    req.session.login == "true";
+server.post('/test', function(req, res) {
+    res.send(200);
+})
 
+server.post('/shopify/webhooks', function(req, res) {
+    console.log(req.query);
     console.log('hello3');
 
-    res.redirect('/');
+    res.send(200);
 });
 
 server.listen(process.env.port || 80, function () {
