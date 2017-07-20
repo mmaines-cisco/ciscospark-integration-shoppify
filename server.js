b17ea48dbf14f2a4362f0c5c0888851d35019267
mmaines-cisco <mmaines@cisco.com>
2017-07-20 14:25:54 -0400

    edit

--- server.js
+++ server.js
@@ -33,14 +33,16 @@ let auth = function(req, res, next) {
     else
     {
         //https://api.ciscospark.com/v1/authorize?response_type=code&client_id=Cf14c66a547d565a58a107141a509e8f4d8a7367e7a9cd9966675387841c41ca9&scope=spark:rooms_read,spark:messages_write&state=youbetcha&redirect_uri=https://requestb.in/zh18zhzh
-        const url = `https://api.ciscospark.com/v1/authorize?response_type=code&client_id=` + env.ciscospark["client-id"] + `&scope=` + env.ciscospark.scope + `&redirect_uri=https://requestb.in/zh18zhzh`;
-        console.log(url);
-        res.redirect(url);
+        //const url = `https://api.ciscospark.com/v1/authorize?response_type=code&client_id=` + env.ciscospark["client-id"] + `&scope=` + env.ciscospark.scope + `&redirect_uri=https://requestb.in/zh18zhzh`;
+        //console.log(url);
+        //res.redirect(url);
+        console.log("HELOOOO");
+        next();
     }
         
 };
 
-server.get('/', function(req,res) {
+server.get('/', auth, function(req,res) {
     if(req.params.code)
         res.send(200);
     else
